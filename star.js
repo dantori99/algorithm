ed98a3cd442ee07988b94d12477dfe0f9da79c32
Daniel Harianja <danieltogarharianja5@gmail.com>
2022-06-27 16:58:17 +0700

    add Star

--- star.js
+++ star.js
@@ -1,5 +1,15 @@
 let n = 5;
 let string = "";
-for (let i = 0; i < n; i++) {
-    
+// External loop
+for (let i = 1; i <= n; i++) {
+  // printing spaces
+  for (let j = 1; j <= n - i; j++) {
+    string += " ";
+  }
+  // printing star
+  for (let k = 0; k < 2 * i - 1; k++) {
+    string += "*";
+  }
+  string += "\n";
 }
+console.log(string);
